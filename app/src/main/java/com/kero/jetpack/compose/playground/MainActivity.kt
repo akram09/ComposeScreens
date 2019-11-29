887fdb8bd7d8374bd1f40ec7bd05cab34c4a53e2
akram09 <a.boutouchent@esi-sba.dz>
2019-11-29 16:28:29 +0100

    add a simple drawer

--- app/src/main/java/com/kero/jetpack/compose/playground/MainActivity.kt
+++ app/src/main/java/com/kero/jetpack/compose/playground/MainActivity.kt
@@ -2,9 +2,13 @@ package com.kero.jetpack.compose.playground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
+import androidx.ui.layout.Column
+import androidx.ui.material.*
+import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +20,16 @@ class MainActivity : AppCompatActivity() {
     }
     @Composable
     fun firstView(){
-        Text("Hello World")
+        Log.e("errr", "entered")
+        MaterialTheme {
+            ModalDrawerLayout(DrawerState.Opened , {
+                Log.e("errr", "changed")
+            }, drawerContent = {
+                Text("Hello World ")
+            }, bodyContent = {
+                Text("heu")
+            })
+
+        }
     }
 }
